0e969dfc5302fab4593c7b4c52fcb4d61c6ce0b5
mmparadinha <matheusmparadinha@gmail.com>
2023-04-21 11:12:56 -0300

    Agora vai. Fix final, realmente precisa dotenv-cli

--- front-end/src/components/App.jsx
+++ front-end/src/components/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <Routes>
         <Route path="/"  element={<Home />} />
         <Route path="/names" element={<Names />} />
-        <Route path="/*" element={<Home />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
